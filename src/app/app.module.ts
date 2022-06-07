0e35a1035a1f06a596a923e38353c8c59576cdbd
Roberto Berrocal <robertoberro20@gmail.com>
2022-06-07 17:57:34 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// external
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
+import { ListaProductoComponent } from './producto/lista-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent,
+    DetalleProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule,
+    AppRoutingModule,
+    ToastrModule.forRoot({
+      timeOut:10000,
+      positionClass:"toast-bottom-right",
+      preventDuplicates:true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
